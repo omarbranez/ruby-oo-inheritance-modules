1031a0d4b904d9ef891df231adf1a84c990a6d7c
Omar Branez <omar.branez@gmail.com>
2021-02-27 14:42:10 -0500

    commit tree 9c9cf6006a474a59c93850466b42fdbf080793da

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,15 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+    include FancyDance::InstanceMethods # _INCLUDE_ allows us to access the FancyDance module to use its InstanceMethods module _INSTANCE_
+    extend FancyDance::ClassMethods # _EXTEND_ allows us to use the FancyDance module to use its ClassMethods module _CLASS_
+
+    attr_accessor :name
+    
+    def initialize(name)
+        @name = name
+    end
+
+end
